218b8b60b20bbae07d3d526f137ebd5128093f80
Hubertas <hubertas.geciauskas@gmail.com>
2024-07-13 19:32:31 +0300

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+
+ <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; Your Website <?php echo date("Y"); ?></p>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+        </footer>
